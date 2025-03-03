e4e817ceb3e844a09778b47c32436775bea0ada8
Sidney Thiel <40369915+CreepySunny@users.noreply.github.com>
2025-03-03 12:42:57 +0100

    Get Tasks from bytes slice

---
+++ src/tasks_test.go
@@ -0,0 +1,31 @@
+package src
+
+import (
+	"reflect"
+	"testing"
+	"time"
+)
+
+func TestParseTasksFromData(t *testing.T) {
+	data := []byte(`ID,Description,CreatedAt,IsComplete
+1,My new task,2024-07-27T16:45:19-05:00,true
+2,Finish this video,2024-07-27T16:45:26-05:00,true
+3,Find a video editor,2024-07-27T16:45:31-05:00,false`)
+
+	got, err := readTasksFromData(data)
+	want := []Task{
+		{ID: 1, Description: "My new task", CreatedAt: time.Date(2024, 7, 27, 16, 45, 19, 0, time.FixedZone("", -5*3600)), IsCompleted: true},
+		{ID: 2, Description: "Finish this video", CreatedAt: time.Date(2024, 7, 27, 16, 45, 26, 0, time.FixedZone("", -5*3600)), IsCompleted: true},
+		{ID: 3, Description: "Find a video editor", CreatedAt: time.Date(2024, 7, 27, 16, 45, 31, 0, time.FixedZone("", -5*3600)), IsCompleted: false},
+	}
+
+	if err != nil {
+		t.Errorf("[!] error [!]: %v", err)
+		return
+	}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got: %v, want: %v", got, want)
+	}
+
+}
